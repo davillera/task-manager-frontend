aea3b27cba165d199b6d30a05fdcdd3efc7c5ecc
Daniel Villera <davillera77@gmail.com>
2024-09-01 14:09:26 -0500

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {importProvidersFrom} from "@angular/core";
 
 import { routes } from './app.routes';
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
+    )
+  ],
+
 };
